3ac63b7d01a3e7fbc49b30974ba90471d9c52477
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2021-05-20 16:08:12 +0500

    FAQ accordion JS fixed

--- public/main.js
+++ public/main.js
@@ -11,6 +11,34 @@ window.onscroll = function () {
 
 document.querySelector('.menu-toggle').addEventListener('click', e => {
     e.preventDefault();
-
     document.querySelector('.nav-menu').classList.toggle('hidden')
 })
+
+
+// =============== FAQ accordion ===============
+const menuLinks = document.querySelectorAll(".accordion button");
+menuLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault()
+        let menuContent = link.nextElementSibling;
+        let active = document.querySelector(".accordion section.active");
+
+        if (menuContent) {
+            if (active) {
+                if (link.parentElement.classList.contains("active")) {
+                    link.parentElement.classList.remove("active");
+                    active.lastElementChild.classList.remove('!max-h-96')
+                } else {
+                    active.classList.remove("active");
+                    active.lastElementChild.classList.remove('!max-h-96')
+                    console.log(active)
+                    link.parentElement.classList.add("active");
+                    menuContent.classList.add('!max-h-96')
+                }
+            } else {
+                link.parentElement.classList.add("active");
+                menuContent.classList.add('!max-h-96')
+            }
+        }
+    });
+});
